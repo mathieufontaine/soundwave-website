255f053b9e9445192e14dc6eee0e54f0f9bf129c
MATHIEU FONTAINE <mathieufontaine@mathieus-macbook-pro.home>
2020-10-25 19:10:14 +0100

    added more animations

--- public/js/main.js
+++ public/js/main.js
@@ -27,47 +27,102 @@ function delay(n) {
 
 const homeAnimation = () => {
   let tl = gsap.timeline();
-  tl.fromTo(
-    ".call-to-action",
-    {
-      duration: 1.5,
-      translateX: "100%",
-      opacity: 0
-    },
-    {
-      duration: 1.5,
-      translateX: 0,
-      opacity: 1
-    }
-  );
-  tl.fromTo(
+
+  tl.from(".call-to-action", {
+    duration: 1.5,
+    translateX: "100%",
+    opacity: 0
+  });
+  tl.from(
     ".img-wrapper",
     { duration: 1.5, translateY: 100, opacity: 0 },
-    {
-      duration: 1.5,
-      translateY: 0,
-      opacity: 1
-    },
     "-=1"
   );
 };
 
-// const logo = document.querySelector(".brand-logo");
+const discoverAnimation = () => {
+  const tl = gsap.timeline();
 
-// logo.addEventListener("click", () => {
-//   homeAnimation();
-// });
+  tl.from(".content", { duration: 1.5, translateY: 50, opacity: 0 });
+  tl.from("img", { duration: 1.5, opacity: 0 }, "-=1");
+};
+
+const joinAnimation = () => {
+  const tl = gsap.timeline();
+
+  tl.from(".title", { duration: 1.5, translateY: 50, opacity: 0 });
+  tl.from(
+    ".form-container",
+    { duration: 1.5, translateY: 50, opacity: 0 },
+    "-=1"
+  );
+};
 
 barba.init({
   sync: true,
 
   transitions: [
     {
+      name: "home-transition",
       async leave() {
         const done = this.async();
         pageTransition();
         await delay(1000);
         done();
+      },
+      async enter() {
+        homeAnimation();
+      },
+      async once() {
+        homeAnimation();
+      },
+      from: {
+        namespace: ["join", "discover"]
+      },
+      to: {
+        namespace: ["home"]
+      }
+    },
+    {
+      name: "discover-transition",
+      async leave() {
+        const done = this.async();
+        pageTransition();
+        await delay(1000);
+        done();
+      },
+      async enter() {
+        discoverAnimation();
+      },
+      async once() {
+        discoverAnimation();
+      },
+      from: {
+        namespace: ["join", "home"]
+      },
+      to: {
+        namespace: ["discover"]
+      }
+    },
+    {
+      name: "join-transition",
+      async leave() {
+        const done = this.async();
+        pageTransition();
+        await delay(1000);
+        done();
+      },
+      async enter() {
+        joinAnimation();
+      },
+      async once() {
+        joinAnimation();
+      },
+      from: {
+        namespace: ["discover", "home"]
+      },
+      to: {
+        namespace: ["join"]
       }
     }
   ]
